e5b6f92ad13bb4d77ad390a67de72a63938991ed
Shawn Furyan <shawn.furyan@gmail.com>
2013-05-02 01:36:47 -0500

    Cleaned up polls.views.index(); improved polls.views.detail() to handle nonexistent poll_id; created scaffold templates/polls/detail.html

--- TDB_1/polls/views.py
+++ TDB_1/polls/views.py
@@ -1,20 +1,19 @@
 from django.http import HttpResponse
-from django.template import Context, loader
+from django.shortcuts import render
 
 from polls.models import Poll
 
 def index(request):
-    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = Context({
-        'latest_poll_list': latest_poll_list,
-    })
-    return HttpResponse(template.render(context))
-    #output = ', '.join([p.question for p in latest_poll_list])
-    #return HttpResponse(output}
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5] # "[:5]" defines number of results to return
+    context = { 'latest_poll_list': latest_poll_list }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
