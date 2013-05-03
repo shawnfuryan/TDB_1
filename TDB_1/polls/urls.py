8ec89760134c1af2a5e915f17147ebabbe028030
Shawn Furyan <shawn.furyan@gmail.com>
2013-05-02 22:23:51 -0500

    refactored polls app to use generic views for index, detail and results views; added generics to urls.py and removed polls.views.[index/detail/results]() leaving polls.views.vote();

--- TDB_1/polls/urls.py
+++ TDB_1/polls/urls.py
@@ -1,14 +1,28 @@
 from django.conf.urls import patterns, url
-
-from polls import views
+from django.views.generic import DetailView, ListView
+from polls.models import Poll
+#from polls.views import vote
 
 urlpatterns = patterns('',
         # eg: /polls/
-        url(r'^$', views.index, name='index'),
+        url(r'^$',
+            ListView.as_view(
+                queryset=Poll.objects.order_by('-pub_date')[:5],
+                context_object_name='latest_poll_list',
+                template_name='polls/index.html'),
+            name='index'),
         # eg: /polls/25/
-        url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
+        url(r'^(?P<pk>\d+)/$',
+            DetailView.as_view(
+                model=Poll,
+                template_name='polls/detail.html'),
+            name='detail'),
         # eg: /polls/25/results/
-        url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
+        url(r'^(?P<pk>\d+)/results/$',
+            DetailView.as_view(
+                model=Poll,
+                template_name='polls/results.html'),
+            name='results'),
         # eg: /polls/25/vote/
-        url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
+        url(r'^(?P<poll_id>\d+)/vote/$', 'polls.views.vote', name='vote'),
 )
--- TDB_1/polls/views.py
+++ TDB_1/polls/views.py
@@ -1,20 +1,7 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.core.urlresolvers import reverse
 from polls.models import Poll
-
-def index(request):
-    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5] # "[:5]" defines number of results to return
-    context = { 'latest_poll_list': latest_poll_list }
-    return render(request, 'polls/index.html', context)
-
-def detail(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/detail.html', {'poll': poll})
-
-def results(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/results.html', {'poll':poll})
 
 def vote(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
