1c9ed10030495aa8a225410ebe072a7752062a21
Shawn Furyan <shawnfuryan@gmail.com>
2013-05-09 17:25:39 -0500

    update settings.local

--- TDB_1/settings/local.py
+++ TDB_1/settings/local.py
@@ -2,6 +2,7 @@ from .base import *
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+INTERNAL_IPS = ('127.0.0.1',)
 
 INSTALLED_APPS += ("debug_toolbar", )
 MIDDLEWARE_CLASSES += ("debug_toolbar.middleware.DebugToolbarMiddleware", )
